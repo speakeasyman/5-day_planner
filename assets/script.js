52dc2c35af1273c8f720520407a79ccaf070e131
Jonathan Nance <jonathan.w.nance@gmail.com>
2021-05-02 13:46:47 -0400

    got it so the schedule will go and pull from local storage

--- assets/script.js
+++ assets/script.js
@@ -16,11 +16,30 @@ $(document).ready(function(){
 
 $('.save').click(function(){
     var schedule = {
-        nine: $('textarea')[0].value,
-        ten: $('textarea')[1].value,
+        nineAm: $('textarea')[0].value,
+        tenAm: $('textarea')[1].value,
+        elevenAm: $('textarea')[2].value,
+        noon: $('textarea')[3].value,
+        onepm: $('textarea')[4].value,
+        twoPm: $('textarea')[5].value,
+        threePm: $('textarea')[6].value,
+        fourPm: $('textarea')[7].value,
+        fivePm: $('textarea')[8].value,
     }
-    
-
-    console.log(schedule);
-    
+    localStorage.setItem('schedule', JSON.stringify(schedule));  
+        console.log(schedule);    
 });
+
+function loadSchedule(){
+    schedule = JSON.parse(localStorage.getItem('schedule'));
+    console.log(schedule);
+    $('#0900').text(schedule.nineAm);
+    $('#1000').text(schedule.tenAm);
+    $('#1100').text(schedule.elevenAm);
+    $('#1200').text(schedule.noon);
+    $('#1300').text(schedule.onepm);
+    $('#1400').text(schedule.twoPm);
+    $('#1500').text(schedule.threePm);
+    $('#1600').text(schedule.fourPm);
+    $('#1700').text(schedule.fivePm);
+}
